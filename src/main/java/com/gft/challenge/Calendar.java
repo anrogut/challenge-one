abd4ebc9e87c9d13e15049150a2a24a57a56d416
anrogut <anrt@gft.com>
2016-10-20 11:15:43 +0200

    Remove not required methods from iterator

--- src/main/java/com/gft/challenge/Calendar.java
+++ src/main/java/com/gft/challenge/Calendar.java
@@ -3,9 +3,6 @@ package com.gft.challenge;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.Iterator;
-import java.util.Spliterator;
-import java.util.Spliterators;
-import java.util.function.Consumer;
 
 
 class Calendar implements Iterable<LocalDate> {
@@ -22,18 +19,6 @@ class Calendar implements Iterable<LocalDate> {
     @Override
     public Iterator<LocalDate> iterator() {
         return new CalendarIterator(startDate);
-    }
-
-    @Override
-    public void forEach(Consumer<? super LocalDate> action) {
-        for (LocalDate localDate : this) {
-            action.accept(localDate);
-        }
-    }
-
-    @Override
-    public Spliterator<LocalDate> spliterator() {
-        return Spliterators.spliteratorUnknownSize(iterator(),0);
     }
 
     private class CalendarIterator implements Iterator<LocalDate> {
--- src/test/java/com/gft/challenge/CalendarTest.java
+++ src/test/java/com/gft/challenge/CalendarTest.java
@@ -3,15 +3,27 @@ package com.gft.challenge;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.util.Spliterator;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class CalendarTest {
 
     private static final String DATE_STRING = "2016-09-19";
     private Calendar calendar;
+
+    private List<LocalDate> correctDates = Arrays.asList(
+            LocalDate.of(2016,9,20),
+            LocalDate.of(2016,9,23),
+            LocalDate.of(2016,9,27),
+            LocalDate.of(2016,9,30)
+    );
 
     @Before
     public void setUp() {
@@ -19,14 +31,41 @@ public class CalendarTest {
     }
 
     @Test
-    public void spliteratorShouldNotBeNull() {
-        assertThat(calendar.spliterator()).isNotNull();
+    public void hasNextShouldReturnTrueWhenStartDateNotNull() {
+        Iterator iterator = calendar.iterator();
+        assertThat(iterator.hasNext()).isTrue();
     }
 
     @Test
-    public void spliteratorIsInstanceOfSpliteratorClass() {
-        assertThat(calendar.spliterator()).isInstanceOf(Spliterator.class);
-        assertThat(calendar.spliterator().estimateSize()).isEqualTo(Long.MAX_VALUE);
+    public void hasNextShouldReturnTrueWhenStartDateIsNull() {
+        Calendar localCalendar = new Calendar(null);
+        Iterator<LocalDate> iterator = localCalendar.iterator();
+        assertThat(iterator.hasNext()).isTrue();
+    }
+
+    @Test
+    public void returnedDateShouldBeMondayOrFriday() {
+        Iterator<LocalDate> iterator = calendar.iterator();
+        assertThat(iterator.next().getDayOfWeek()).isIn(DayOfWeek.FRIDAY,DayOfWeek.TUESDAY);
+        assertThat(iterator.next().getDayOfWeek()).isIn(DayOfWeek.FRIDAY,DayOfWeek.TUESDAY);
+    }
+
+    @Test
+    public void shouldBeAbleToIterateOverFewMondaysAndFridays() {
+        Iterator<LocalDate> iterator = calendar.iterator();
+        List<LocalDate> datesFromIterator = correctDates
+                .stream().map(date -> iterator.next())
+                .collect(Collectors.toList());
+
+        assertThat(datesFromIterator).hasSameElementsAs(correctDates);
+    }
+
+    @Test
+    public void shouldThrowUnsupportedOperationException() {
+        Iterator iterator = calendar.iterator();
+        assertThatExceptionOfType(UnsupportedOperationException.class)
+                .isThrownBy(iterator::remove)
+                .withMessage("Not able to remove date");
     }
 
 }
